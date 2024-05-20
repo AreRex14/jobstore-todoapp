d5142f16d67f270e324636efd33655880a1abb0e
AreRex14 <rexcode14@gmail.com>
2024-05-20 08:04:49 +0800

    create and import TodoForm and TodoList to TodoApp

---
+++ resources/js/Components/Todo/TodoApp.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TodoForm from "@/Components/Todo/TodoForm.vue";
+import TodoList from "@/Components/Todo/TodoList.vue";
+
+</script>
+
+<template>
+    <div>
+        <h1>Todos</h1>
+        <TodoForm />
+        <TodoList />
+    </div>
+</template>
