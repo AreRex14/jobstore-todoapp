31b7e751f686b6cdbc321ab5fd53a4b16ac8d848
AreRex14 <rexcode14@gmail.com>
2024-05-20 08:02:56 +0800

    create useTodosStore pina store

---
+++ resources/js/Stores/useTodosStore.js
@@ -0,0 +1,47 @@
+import { defineStore } from "pinia";
+
+export const useTodosStore = defineStore("todos", {
+    state: () => ({
+        todos: [],
+        id: 0,
+        showAlert: false,
+    }),
+    actions: {
+        addTodo(todo) {
+            const newTodo = {
+                id: ++this.id,
+                title: todo,
+                is_completed: false,
+                created_at: new Date().toLocaleString(),
+            }
+            this.todos.unshift(newTodo);
+            this.saveTodos();
+        },
+        saveTodos() {
+            sessionStorage.setItem('todos', JSON.stringify(this.todos))
+        },
+        loadTodos() {
+            const todos = sessionStorage.getItem('todos')
+            if (todos) {
+              this.todos = JSON.parse(todos)
+            }
+          },
+        deleteTodo(id) {
+            this.todos = this.todos.filter((todo) => todo.id !== id);
+        },
+        toggleTodo(id) {
+            this.todos = this.todos.map((todo) => {
+                if (todo.id === id) {
+                    todo.is_completed = !todo.is_completed;
+                }
+                return todo;
+            });
+        },
+        toggleShowAlert() {
+            this.showAlert = true;
+            setTimeout(() => {
+                this.showAlert = false;
+            }, 3000);
+        },
+    },
+});
